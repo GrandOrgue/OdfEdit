30a2227d5c75d2943110e7eb098bb8dad217b9e3
Denis Roussel <denis.roussel@acsone.eu>
2022-07-15 18:32:38 +0200

    [FIX] Improve gitignore and setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 setup(
     version="0.0.1",
     name="OdfEdit",
     description="ODF Edition Tool",
-    packages=["src"],
+    packages=find_packages(),
     include_package_data=True,
     package_data={'': ['*.png', 'OdfEdit_res/*.png']},
     entry_points={
-        'console_scripts': ['OdfEdit=src:OdfEdit.main']
+        'console_scripts': ["OdfEdit = src:OdfEdit.main"]
     }
 )
